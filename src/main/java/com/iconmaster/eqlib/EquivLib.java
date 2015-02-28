cd413a55f553fa597eed815ac15df57bb84d32b0
Iconmaster <crazybonesj@yahoo.com>
2015-02-28 11:06:01 -0500

    Initial commit

---
+++ src/main/java/com/iconmaster/eqlib/EquivLib.java
@@ -0,0 +1,16 @@
+package com.iconmaster.eqlib;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ *
+ * @author iconmaster
+ */
+@Mod(modid = EquivLib.MODID, version = EquivLib.VERSION)
+public class EquivLib {
+	public static final String MODID = "EquivLib";
+	public static final String VERSION = "@VERSION@";
+	
+	@Mod.Instance("EquivLib")
+	public static EquivLib instance = new EquivLib();
+}
