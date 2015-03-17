eb168cab8f6723e63e49f97c86884534f57292e9
Iconmaster <crazybonesj@yahoo.com>
2015-03-17 18:00:32 -0400

    Added oredict blacklist

--- src/main/java/com/iconmaster/eqlib/recipe/handler/OreDictHandler.java
+++ src/main/java/com/iconmaster/eqlib/recipe/handler/OreDictHandler.java
@@ -3,6 +3,7 @@ package com.iconmaster.eqlib.recipe.handler;
 import com.iconmaster.eqlib.recipe.ItemDataStack;
 import com.iconmaster.eqlib.recipe.RecipeHandler;
 import com.iconmaster.eqlib.recipe.RecipeLink;
+import com.iconmaster.eqlib.recipe.RecipeRegistry;
 import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.item.ItemStack;
@@ -19,15 +20,24 @@ public class OreDictHandler implements RecipeHandler {
 		List<RecipeLink> a = new ArrayList<RecipeLink>();
 		
 		for (String s : OreDictionary.getOreNames()) {
+			boolean blacklisted = false;
+			for (String black : RecipeRegistry.oreDictBlacklist) {
+				if (black.equals(s)) {
+					blacklisted = true;
+				}
+			}
+			
+			if (!blacklisted) {
 			for (ItemStack item1 : OreDictionary.getOres(s)) {
-				for (ItemStack item2 : OreDictionary.getOres(s)) {
-					if (item1!=item2) {
-						RecipeLink link = new RecipeLink();
-						
-						link.inputs.add(new ItemDataStack(item1));
-						link.outputs.add(new ItemDataStack(item2));
-						
-						a.add(link);
+					for (ItemStack item2 : OreDictionary.getOres(s)) {
+						if (item1!=item2) {
+							RecipeLink link = new RecipeLink();
+
+							link.inputs.add(new ItemDataStack(item1));
+							link.outputs.add(new ItemDataStack(item2));
+
+							a.add(link);
+						}
 					}
 				}
 			}
